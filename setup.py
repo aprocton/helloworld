3f15a176887a4747fa9617d4ed2b8bf125aa9e41
Alex Procton <aprocton@gmail.com>
2018-02-22 15:50:39 -0500

    updated setup.py to be fancier

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setup(
     name="mypackage",
     version="0.1",
     packages=["helloworld"],
+    author="Alexander Procton",
+    license="GPLv3",
     entry_points={
         'console_scripts': ['helloworld = helloworld.__main__:main']
-        }
+        },
+    classifiers=["Programming Language :: Python :: 3"],
 )
 
